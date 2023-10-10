92bcb1c513e200828a4bd072f99fe79dcee43f28
dedelduwell <ardheliamputri@gmail.com>
2023-10-10 12:59:44 +0700

    Revisi Terbaru -Della

--- Booking_Rumahsakit.java
+++ Booking_Rumahsakit.java
@@ -40,18 +40,19 @@ Scanner sc = new Scanner (System.in);
         System.out.print("Jenis Pemeriksaan :");
         JenisPemeriksaan = sc.next();
         System.out.print("Gejala Pasien :");
-        GejalaPasien = sc.next();
+        GejalaPasien = sc.nextLine();
+        sc.nextLine();
         System.out.print("Apakah penyakit parah? (ya/tidak): ");
         String penyakitParahInput = sc.next();
         penyakitParah = penyakitParahInput.equalsIgnoreCase("ya");
 
         //Pemilihan tipe kamar
         if (penyakitParah) {
-            System.out.print("Penyakit dianggap parah. Pilih tipe kamar yang tersedia (VIP/Kelas1/Kelas2) : ");
+            System.out.print("Penyakit dianggap parah. Pilih tipe kamar yang tersedia (Kelas 1 / Kelas 2 / VIP ) : ");
             TipeKamar = sc.next();
         } else {
             System.out.print("Penyakit tidak dianggap parah. Silahkan ambil obat di apotik. ");
-           TipeKamar = "Tidak Ada"; //Pemilihan kamar tifak berlaku jika penyait tidak parah 
+           TipeKamar = "Tidak Ada"; //Pemilihan kamar tidak berlaku jika penyait tidak parah 
         }
         
           System.out.println("");
@@ -60,12 +61,12 @@ Scanner sc = new Scanner (System.in);
 
      // Proses pemberian nomor antrian berdasarkan jumlah pasien yang mendaftar sebelumnya
      // Contoh sederhana: nomor_antrian = jumlah_pasien_sebelumnya + 1
-     int jumlahPasienSebelumnya = 1;
-     int nomorAntrian = jumlahPasienSebelumnya + 1;
+     //int jumlahPasienSebelumnya = 1;
+     //int nomorAntrian = jumlahPasienSebelumnya + 1;
 
      //Output nomor antrian dan data pasien yang terdaftar
      System.out.println("\n=== DETAIL PEMESANAN ===\n");
-     System.out.println("Nomor Antrian: " + nomorAntrian);
+     //System.out.println("Nomor Antrian: " + nomorAntrian);
      System.out.println("Data Pasien:");
      System.out.println("Nama: " + NamaPasien);
      System.out.println("Nomor Identitas: " + NomorIdentitas);
@@ -82,34 +83,37 @@ Scanner sc = new Scanner (System.in);
      }
      
 // Perhitungan biaya rawat inap
-int biayaPerHari;
-if (TipeKamar.equalsIgnoreCase("Kelas1")) {
-biayaPerHari = 300000;
-} else if (TipeKamar.equalsIgnoreCase("Kelas2")) {
-    biayaPerHari = 250000;
-} else if (TipeKamar.equalsIgnoreCase("Kelas3")) {
-    biayaPerHari = 100000;
-} else {
-    biayaPerHari = 100000; //Default biaya jika tipe kamar tidak valid
+int totalPembayaran = 0;
+if (penyakitParah) {
+    int biayaPerHari;
+    if (TipeKamar.equalsIgnoreCase("Kelas 1")) {
+        biayaPerHari = 300000;
+    } else if (TipeKamar.equalsIgnoreCase("Kelas 2")) {
+        biayaPerHari = 250000;
+    } else if (TipeKamar.equalsIgnoreCase("VIP")) {
+        biayaPerHari = 500000;
+    } else {
+        biayaPerHari = 0; 
+    }
+System.out.println("Masukkan lama rawat inap(Hari)");
+int lamaRawatInap = sc.nextInt();
+totalPembayaran = biayaPerHari * lamaRawatInap;
 }
 
 
 
-     //int biaya1hari, lamarawatinap, totalpembayaran;
-     //biaya1hari= 200000;
      
-int lamaRawatInap, totalPembayaran;
-System.out.print("Masukkan lama rawat inap (Hari): ");
-lamaRawatInap = sc.nextInt();
 
-     //System.out.println("masukkan lama rawat inap : ");
-     //lamarawatinap= sc.nextInt();
+System.out.println("*******************************");
+if (penyakitParah) {
+    System.out.println(" Total Biaya Rumah Sakit : " + totalPembayaran);
+} else {
+    System.out.println("Pasien tidak perlu membayar rawat inap karena penyakit tidak parah, maka langsung menuju bagian apotik.");
+}
 
-     totalPembayaran= biayaPerHari*lamaRawatInap;
 
-     System.out.println("**************************");
-     System.out.println("Total Biaya Rumah Sakit :  " + totalPembayaran);
-     
+
+    
     }
 }
     
