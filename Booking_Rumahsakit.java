b666740e53a41a7997d3f34cdcc29c3d6d9e3134
dedelduwell <ardheliamputri@gmail.com>
2023-10-24 16:24:00 +0700

    Tugas Jobsheet 5 dan 6

--- Booking_Rumahsakit.java
+++ Booking_Rumahsakit.java
@@ -8,27 +8,14 @@ Scanner sc = new Scanner (System.in);
         String NamaPasien, Tempat, TanggalLahir, alamat, NomorTelepon, JenisPemeriksaan, GejalaPasien, TipeKamar;
         long NomorIdentitas;
         boolean penyakitParah = false;
-        
-        
-
-        // Isi Variabel
-        //String BiayaRawatInap = "250000";
 
         System.out.print("Nama Pasien :");
         NamaPasien = sc.nextLine();
-        System.out.print("Tempat :");
+        System.out.print("Masukkan Tempat Lahir :");
         Tempat = sc.next();
         System.out.print("Tanggal Lahir (format : dd/mm/yyyy): ");
-        String rawTanggalLahir = sc.next();
-        // Validasi Tanggal Lahir 
-        if (rawTanggalLahir.matches("\\d{2}/\\d{2}/\\d{4}")) {
-            TanggalLahir = rawTanggalLahir;
-        } else {
-            System.out.println("Format tanggal lahir tidak valid.");
-        }
-
-        sc.nextLine();
-
+        TanggalLahir = sc.nextLine();
+      //??????????
         System.out.print("Alamat :");
         alamat = sc.nextLine();
         System.out.print("Nomor Telepon :");
@@ -41,8 +28,8 @@ Scanner sc = new Scanner (System.in);
         JenisPemeriksaan = sc.next();
         System.out.print("Gejala Pasien :");
         GejalaPasien = sc.nextLine();
-        sc.nextLine();
-        System.out.print("Apakah penyakit parah? (ya/tidak): ");
+    
+        System.out.print("Apakah penyakit parah? (ya/tidak) ? : ");
         String penyakitParahInput = sc.next();
         penyakitParah = penyakitParahInput.equalsIgnoreCase("ya");
 
@@ -58,23 +45,21 @@ Scanner sc = new Scanner (System.in);
           System.out.println("");
           System.out.println("");
           System.out.println("*****************");
+          System.out.println("*****************");
 
-     // Proses pemberian nomor antrian berdasarkan jumlah pasien yang mendaftar sebelumnya
-     // Contoh sederhana: nomor_antrian = jumlah_pasien_sebelumnya + 1
-     //int jumlahPasienSebelumnya = 1;
-     //int nomorAntrian = jumlahPasienSebelumnya + 1;
+     
 
-     //Output nomor antrian dan data pasien yang terdaftar
      System.out.println("\n=== DETAIL PEMESANAN ===\n");
-     //System.out.println("Nomor Antrian: " + nomorAntrian);
-     System.out.println("Data Pasien:");
+     System.out.println("\n=======Data Pasien:=====\n");
      System.out.println("Nama: " + NamaPasien);
      System.out.println("Nomor Identitas: " + NomorIdentitas);
      System.out.println("Alamat: " + alamat);
      System.out.println("Nomor Telepon: " + NomorTelepon);
      System.out.println("Tempat : " + Tempat);
-     System.out.println("Tanggal Lahir :" + rawTanggalLahir);
+     System.out.println("Tanggal Lahir :" );
      System.out.println("Gejala Pasien: " + GejalaPasien);
+     
+     
      if (penyakitParah) {
         System.out.println("Apakah penyakit parah? Ya");
         System.out.println("Tipe Kamar: " + TipeKamar);
